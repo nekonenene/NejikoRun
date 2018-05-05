7ca41716ed5b1a6e7aef88193f1a30dab5157c46
ハトネコエ (nekonenene) <hato.nekoe@gmail.com>
2018-05-06 04:59:41 +0900

    自動で走行するように

--- Assets/NejikoController.cs
+++ Assets/NejikoController.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NejikoController : MonoBehaviour {
+	const int MinLane = -2;
+	const int MaxLane = 2;
+	const float LaneWidth = 1.0f;
+
 	CharacterController controller;
 	Animator animator;
 
 	Vector3 moveDirection = Vector3.zero;
+	int targetLane;
 
 	public float gravity;
-	public float speedZ;
+	public float speedX; // 横方向の移動スピード
+	public float speedZ; // 奥への移動スピード
+	public float accelerationZ; // 奥方向への加速度
 	public float speedJump;
 
 	// Use this for initialization
@@ -18,27 +25,21 @@ public class NejikoController : MonoBehaviour {
 		controller = GetComponent<CharacterController>();
 		animator = GetComponent<Animator>();
 	}
-	
+
 	// Update is called once per frame
 	void Update() {
-		// 地上にいる場合のみ操作を受け付ける
-		if(controller.isGrounded) {
-			// Input（タテ）を検知して前へ
-			if(Input.GetAxis("Vertical") > 0.0f) {
-				moveDirection.z = Input.GetAxis("Vertical") * speedZ;
-			} else {
-				moveDirection.z = 0;
-			}
+		if(Input.GetKeyDown("left")) MoveLane(toRight: false);
+		if(Input.GetKeyDown("right")) MoveLane(toRight: true);
+		if(Input.GetKeyDown("space")) Jump();
 
-			// Input（横）に応じて方向転換
-			transform.Rotate(0, Input.GetAxis("Horizontal") * 3, 0);
+		// ターゲットレーンに向かってX座標移動
+		float ratioX = (targetLane * LaneWidth - transform.position.x) / LaneWidth; // 現在地とターゲットが離れてるほど値を大きく
+		moveDirection.x = ratioX * speedX;
 
-			// ジャンプ
-			if(Input.GetButton("Jump")) {
-				moveDirection.y = speedJump;
-				animator.SetTrigger("jump"); // アニメーションクリップの切り替え
-			}
-		}
+		// 奥向きへ、一定速度になるまで加速させる
+		float ratioZ = moveDirection.z + (accelerationZ * Time.deltaTime);
+		// Mathf.Clamp : 第一引数の値が、第二引数と第三引数の間を出ないように調整してくれる関数
+		moveDirection.z = Mathf.Clamp(ratioZ, 0, speedZ);
 
 		// 重力を常に与える
 		// Time.deltaTime : 前フレームからの経過時間
@@ -56,5 +57,25 @@ public class NejikoController : MonoBehaviour {
 
 		// 速度が 0 以上なら、アニメーションクリップは run に
 		animator.SetBool("run", moveDirection.z > 0.0f);
+	}
+
+	// レーン移動
+	public void MoveLane(bool toRight = true) {
+		// 地上にいる場合のみ操作を受け付ける
+		if(controller.isGrounded) {
+			if(toRight && targetLane < MaxLane) {
+				++targetLane;
+			} else if(!toRight && MinLane < targetLane) {
+				--targetLane;
+			}
+		}
+	}
+
+	public void Jump() {
+		// 地上にいる場合のみ操作を受け付ける
+		if(controller.isGrounded) {
+			moveDirection.y = speedJump;
+			animator.SetTrigger("jump"); // アニメーションクリップの切り替え
+		}
 	}
 }
