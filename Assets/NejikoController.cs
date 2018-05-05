02138ae4b5af94a4d769628bb8bc793c8ad77689
ハトネコエ (nekonenene) <hato.nekoe@gmail.com>
2018-05-06 04:22:34 +0900

    カメラが追従して動くところまで

---
+++ Assets/NejikoController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NejikoController : MonoBehaviour {
+	CharacterController controller;
+	Animator animator;
+
+	Vector3 moveDirection = Vector3.zero;
+
+	public float gravity;
+	public float speedZ;
+	public float speedJump;
+
+	// Use this for initialization
+	void Start() {
+		// 必要なコンポーネントを自動取得
+		controller = GetComponent<CharacterController>();
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		// 地上にいる場合のみ操作を受け付ける
+		if(controller.isGrounded) {
+			// Input（タテ）を検知して前へ
+			if(Input.GetAxis("Vertical") > 0.0f) {
+				moveDirection.z = Input.GetAxis("Vertical") * speedZ;
+			} else {
+				moveDirection.z = 0;
+			}
+
+			// Input（横）に応じて方向転換
+			transform.Rotate(0, Input.GetAxis("Horizontal") * 3, 0);
+
+			// ジャンプ
+			if(Input.GetButton("Jump")) {
+				moveDirection.y = speedJump;
+				animator.SetTrigger("jump"); // アニメーションクリップの切り替え
+			}
+		}
+
+		// 重力を常に与える
+		// Time.deltaTime : 前フレームからの経過時間
+		// これをかけることによって、異なるフレームレートのデバイスでも一定の割合の値を加えられるようになる（らしい……）
+		moveDirection.y -= gravity * Time.deltaTime;
+
+		// 移動実行
+		Vector3 globalDirection = transform.TransformDirection(moveDirection);
+		controller.Move(globalDirection * Time.deltaTime);
+
+		// 移動後接地しているなら、Y方向の速度（重力）はリセット
+		if(controller.isGrounded) {
+			moveDirection.y = 0;
+		}
+
+		// 速度が 0 以上なら、アニメーションクリップは run に
+		animator.SetBool("run", moveDirection.z > 0.0f);
+	}
+}
---
+++ Assets/SimpleFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleFollow : MonoBehaviour {
+	Vector3 distanceWhenStart;
+
+	public GameObject target;
+	public float followSpeed;
+
+	void Start() {
+		distanceWhenStart = target.transform.position - transform.position;
+	}
+	
+	// Update関数の直後に呼ばれる
+	void LateUpdate() {
+		// Vector3.Lerp は線形補間関数。第一引数と第二引数間で、第三引数の割合に相当する位置を返す
+		transform.position = Vector3.Lerp(
+			transform.position,
+			target.transform.position - distanceWhenStart,
+			Time.deltaTime * followSpeed
+		);
+	}
+}
