4203f5c9c514a156eb86f490462af67ce36c01c3
ハトネコエ (nekonenene) <hato.nekoe@gmail.com>
2018-05-07 06:47:57 +0900

    タイトル画面からメインにシーン遷移されるように

---
+++ Assets/Scripts/TitleController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleController : MonoBehaviour {
+	public void OnStartButtonClicked() {
+		// Application.LoadLevel は deprecated になったので代わりに以下
+		UnityEngine.SceneManagement.SceneManager.LoadScene("Main");
+	}
+}
