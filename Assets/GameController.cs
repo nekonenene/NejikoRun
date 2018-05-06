1bde83c867f07351bd574c614bfcecf33e6b40b4
ハトネコエ (nekonenene) <hato.nekoe@gmail.com>
2018-05-07 04:41:51 +0900

    スコア表示とライフ表示を追加。ステージ難易度を上げた

---
+++ Assets/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public NejikoController nejiko;
+	public Text scoreLabel;
+	public LifePanel lifePanel;
+
+	// Use this for initialization
+	void Start() {
+		
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		int score = CalcScore();
+		scoreLabel.text = "Score: " + score + "m";
+
+		lifePanel.UpdateLife(nejiko.Life());
+	}
+
+	int CalcScore() {
+		// 0地点からの走行距離をスコアとする
+		return (int)nejiko.transform.position.z;
+	}
+}
---
+++ Assets/LifePanel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifePanel : MonoBehaviour {
+
+	public GameObject[] lifeIcons;
+
+	// Use this for initialization
+	void Start() {
+		
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		
+	}
+
+	// 残りライフに応じてスプライトの表示を決める
+	public void UpdateLife(int life) {
+		for(int i = 0; i < lifeIcons.Length; ++i) {
+			if(i < life) {
+				lifeIcons[i].SetActive(true);
+			} else {
+				lifeIcons[i].SetActive(false);
+			}
+		}
+	}
+}
